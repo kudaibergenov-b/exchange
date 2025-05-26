666101764f1491730870075c4890fe7d9a2e2bf5
kudaibergenov-b <kudaibergenov_b@auca.kg>
2025-05-27 02:06:39 +0600

    CORS config

--- src/main/java/com/kudaibergenov/exchange/config/WebConfig.java
+++ src/main/java/com/kudaibergenov/exchange/config/WebConfig.java
@@ -10,4 +10,13 @@ public class WebConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new RequestLoggingInterceptor());
     }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
 }
